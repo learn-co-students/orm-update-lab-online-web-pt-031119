bb8cda49f1bbb877162f9e492b0c3e3c289c9a35
Patrick A <akolo@hotmail.com>
2019-07-13 22:03:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
-attr_accessor :name, :grade
-attr_reader :id 
+attr_accessor :name, :grade, :id 
   
   def initialize ( id=nil, name, grade)
     @id = id 
@@ -14,7 +13,7 @@ attr_reader :id
     @grade = grade 
   end 
     
-  def create_table
+  def self.create_table
     sql = <<-SQL
      CREATE TABLE IF NOT EXISTS students (
           id INTEGER PRIMARY KEY,
@@ -40,23 +39,22 @@ attr_reader :id
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
+    
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
   
-  def self.create (name:, grade:)
+  def self.create (name, grade)
     student = Student.new(name, grade)
     student.save
-    student
   end 
   
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end 
   
   def self.find_by_name (name)
@@ -69,12 +67,10 @@ attr_reader :id
   
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
-    end
   end
   
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-end
 end
